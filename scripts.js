68320bb66926d59bb114920a487d3f9b999dfbf1
DaggerBourne <valentino@daghertech.com>
2021-09-13 04:02:57 -0500

    Created event listeners for keypad, adjusted markup to seperate between different key types

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
-const keys = document.querySelector(".calcbox");
+// const keys = document.querySelectorAll("button");
+const numberKeys = document.querySelectorAll(".number");
+const oppKeys = document.querySelectorAll(".operator")
+const equalKey = document.querySelectorAll(".equal-sign")
+const clearKey = document.querySelectorAll('.all-clear')
 
-console.log(keys)
+// function eventListnerAttacher(button){
+// button.addEventListener('click', handleEvent)
+// };
+
+numberKeys.forEach((numberGetter) => numberGetter.addEventListener('click', valueExtrator));
+// oppKeys.forEach(eventListnerAttacher);
+// equalKey.forEach(eventListnerAttacher);
+// clearKey.forEach(eventListnerAttacher);
+
+
+
+// function numberGetter(numberButton){
+// numberButton.addEventListener('click', valueExtrator)
+// }
+
+
+function valueExtrator(e){
+    console.log(e.currentTarget.value)
+}
