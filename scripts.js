0fbe3d7161ef9094d6f71bf76cf2cc6e0eeee4f7
daggerbourne <valentino@daghertech.com>
2021-09-13 22:32:34 -0500

    joined array for display- will need to change values from string to numbers or detectect string as a regex

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ const equalKey = document.querySelectorAll(".equal-sign")
 const clearKey = document.querySelectorAll('.all-clear')
 
 const calcInputs = [];
+let displayValue = ""
 
 
 numberKeys.forEach((valueGetter) => valueGetter.addEventListener('click', valueExtrator));
@@ -16,13 +17,15 @@ clearKey.forEach((valueGetter) => valueGetter.addEventListener('click', valueExt
 
 
 function valueExtrator(e){
-    console.log(e.currentTarget.value)
+
+  
+    console.log(typeof e.currentTarget.value)
     const keyPad = e.currentTarget.value;
     const calcInput = [
         keyPad
     ]
     calcInputs.push(calcInput);
+    displayValue = calcInputs.join('');
 
-}
+ }
 
-console.log(calcInputs)
