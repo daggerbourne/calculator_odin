619b40f4dc04a8dfe33d106301a0b8f0364aae96
DaggerBourne <valentino@daghertech.com>
2021-09-14 22:07:04 -0500

    started working on accumulator

--- scripts.js
+++ scripts.js
@@ -6,8 +6,9 @@ const clearKey = document.querySelectorAll('.all-clear')
 const display = document.querySelector('.display')
 
 let calcInputs = [];
-let displayValue = ""
-let valueTotal = 0
+let firstNumber = ""
+let keyInput = []
+let runningTotalArr = []
 
 
 numberKeys.forEach((valueGetter) => valueGetter.addEventListener('click', valueExtrator));
@@ -16,29 +17,43 @@ equalKey.forEach((valueGetter) => valueGetter.addEventListener('click', valueExt
 clearKey.forEach((valueGetter) => valueGetter.addEventListener('click', valueExtrator));
 
 
+function tallyTotal(tally, currentTotal){
+    return tally + currentTotal
+}
+
 
 
 function valueExtrator(e){
 
     if(e.currentTarget.className === 'number'){
     console.log(`you clicked a ${e.currentTarget.className}`)
-    // const keyPad = e.currentTarget.value;
+
     const calcInput = [
         e.currentTarget.value
     ]
     calcInputs.push(calcInput);
-    displayValue = calcInputs.join('');
-    display.innerHTML = `"\n                <input type=\"text\" class=\"calculator-screen\" value=\"${displayValue}\" disabled=\"\">\n            "`
-    valueTotal = parseInt(displayValue);
-    console.log(valueTotal);
+    firstNumber = calcInputs.join('');
+    display.innerHTML = `"\n                <input type=\"text\" class=\"calculator-screen\" value=\"${firstNumber}\" disabled=\"\">\n            "`
+    keyInput = [parseInt(firstNumber)];
+    console.log(`the key input is ${keyInput}`);
     }
 
     if(e.currentTarget.className === 'all-clear'){
-        valueTotal = 0
+        keyInput = 0
         displayValue = "0"
         display.innerHTML = `"\n                <input type=\"text\" class=\"calculator-screen\" value=\"${displayValue}\" disabled=\"\">\n            "`
         calcInputs = []
-        console.log(valueTotal)
+        console.log(keyInput)
+    }
+
+    if(e.currentTarget.className === 'operator'){
+        if (e.currentTarget.value === '+'){
+            runningTotalArr.push(keyInput)
+            runningTotalArr.reduce(tallyTotal, 0)
+            
+        }
+
+        
     }
 
 
