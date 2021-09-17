f0e85a49329e3c7313f5ac69b279dbdeb34b18f7
daggerbourne <valentino@daghertech.com>
2021-09-17 16:33:50 -0500

    cleaned up code by creating util functions, got basic addition working in logic, still need to clean up display and figure out why a leading 0 appears

--- scripts.js
+++ scripts.js
@@ -1,29 +1,44 @@
-// const keys = document.querySelectorAll("button");
+//Query Selectors
 const numberKeys = document.querySelectorAll(".number");
 const oppKeys = document.querySelectorAll(".operator")
 const equalKey = document.querySelectorAll(".equal-sign")
 const clearKey = document.querySelectorAll('.all-clear')
 const display = document.querySelector('.display')
 
+//Global variables for calculator
 let calcInputs = [];
-let firstNumber = ""
+let displayValue = "0"
 let keyInput = []
 let runningTotalArr = []
 
+//Event Listners
+numberKeys.forEach((valueGetter) => valueGetter.addEventListener('click', numberCruncher));
+oppKeys.forEach((valueGetter) => valueGetter.addEventListener('click', numberCruncher));
+equalKey.forEach((valueGetter) => valueGetter.addEventListener('click', numberCruncher));
+clearKey.forEach((valueGetter) => valueGetter.addEventListener('click', numberCruncher));
 
-numberKeys.forEach((valueGetter) => valueGetter.addEventListener('click', valueExtrator));
-oppKeys.forEach((valueGetter) => valueGetter.addEventListener('click', valueExtrator));
-equalKey.forEach((valueGetter) => valueGetter.addEventListener('click', valueExtrator));
-clearKey.forEach((valueGetter) => valueGetter.addEventListener('click', valueExtrator));
-
-
+//Addition accumulator 
 function tallyTotal(tally, currentTotal){
+    tally = parseInt(tally);
+    currentTotal = parseInt(currentTotal);
+    console.log(`the tally is ${tally}`)
+    console.log(`the current Total is is ${currentTotal}`)
     return tally + currentTotal
 }
 
+// Function to change text in display of calc
+function userDisplay(text){
+    display.innerHTML = `"\n                <input type=\"text\" class=\"calculator-screen\" value=\"${text}\" disabled=\"\">\n            "`
+}
 
+function resetTallyInput(input){
+    calcInputs = [`${input}`]
+    displayValue = `${input}`
+    keyInput = input
+}
 
-function valueExtrator(e){
+//Main function for dealing with inputs
+function numberCruncher(e){
 
     if(e.currentTarget.className === 'number'){
     console.log(`you clicked a ${e.currentTarget.className}`)
@@ -32,27 +47,35 @@ function valueExtrator(e){
         e.currentTarget.value
     ]
     calcInputs.push(calcInput);
-    firstNumber = calcInputs.join('');
-    display.innerHTML = `"\n                <input type=\"text\" class=\"calculator-screen\" value=\"${firstNumber}\" disabled=\"\">\n            "`
-    keyInput = [parseInt(firstNumber)];
+    displayValue = calcInputs.join('');
+    userDisplay(displayValue);
+    keyInput = [parseInt(displayValue)];
     console.log(`the key input is ${keyInput}`);
     }
+    
 
     if(e.currentTarget.className === 'all-clear'){
-        keyInput = 0
-        displayValue = "0"
-        display.innerHTML = `"\n                <input type=\"text\" class=\"calculator-screen\" value=\"${displayValue}\" disabled=\"\">\n            "`
-        calcInputs = []
-        console.log(keyInput)
-    }
+        resetTallyInput(0)
+        userDisplay(0);
+
+     }
 
     if(e.currentTarget.className === 'operator'){
         if (e.currentTarget.value === '+'){
+            console.log(keyInput)
+            console.log(`the running total is currently ${runningTotalArr}`)
             runningTotalArr.push(keyInput)
-            runningTotalArr.reduce(tallyTotal, 0)
             
-        }
+            let total = runningTotalArr.reduce(tallyTotal, 0)
+            console.log(`the running total is currently ${total}`)
 
+            resetTallyInput(0);
+            userDisplay('+');
+                                 
+        }
+    if(e.currentTarget.className === '.equal-sign'){
+        runningTotalArr.push(keyInput);
+    }
         
     }
 
